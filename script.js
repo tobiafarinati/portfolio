a55dcdeac19b57d45209cc0a00df2ed610db7aa4
tobiafarinati <132692416+tobiafarinati@users.noreply.github.com>
2023-07-22 17:03:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,12 @@ $(document).ready(function () {
 		}
 	);
 });
+
+
+function swipe(event) {
+	if (event.target.className === "modal") {
+	  if (event.touches[0].pageX > window.innerWidth / 2) {
+		event.target.style.display = "none";
+	  }
+	}
+  }
